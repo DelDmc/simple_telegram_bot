242c1773e263c22b076ff6072dd1369a375f044e
K.S <del12dmc@gmail.com>
2022-08-10 18:04:53 +0300

    Bot rebuild. Changed structure and keyboards - added Main menu - added separated menu for Statements

--- bot.py
+++ bot.py
@@ -15,24 +15,29 @@ URL = "https://simple-fin-telegram-bot.herokuapp.com/"
 bot.set_my_commands(
     [
         telebot.types.BotCommand("/start", "Главное меню"),
-        telebot.types.BotCommand("/rates", "Курс валют"),
-        telebot.types.BotCommand("/authorization", "Запрос авторизации"),
-        telebot.types.BotCommand("/balance", "Состояние счета"),
-        telebot.types.BotCommand("/statement", "Приход/Расход за текущий месяц")
+        telebot.types.BotCommand("/help", "Описание"),
     ]
 )
 
 
 deny_icon = u"\u274C"
 accept_icon = u"\u2705"
-options_list = {"rates": "Курс валют", "balance": "Остаток на счету", "statement": "Выписка за текущий месяц"}
 authorization_list = {"accept": accept_icon, "deny": deny_icon}
 
 
 def make_keyboard(options, args=None):
     markup = types.InlineKeyboardMarkup()
     for key, value in options.items():
+
         markup.add(types.InlineKeyboardButton(text=value, callback_data=f"{key} {args}"))
+    return markup
+
+
+def make_keyboard_for_unauthorized():
+    markup = types.ReplyKeyboardMarkup()
+    rate_btn = types.KeyboardButton("Курс валюты")
+    authorization_btn = types.KeyboardButton("Авторизация")
+    markup.row(rate_btn, authorization_btn)
     return markup
 
 
@@ -42,6 +47,7 @@ def make_first_user_superuser():
     user.save()
 
 
+@bot.message_handler(func=lambda message: message.text == 'Меню Старт')
 @bot.message_handler(commands=["start"])
 def start_handler(message):
     if not User.select().where(User.chat_id == message.chat.id):
@@ -51,31 +57,29 @@ def start_handler(message):
             username=message.chat.username
         )
     make_first_user_superuser()
+    user = User.select().where(User.chat_id == message.chat.id).get(db)
+    if user.is_authorized == 1:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        rate_btn = types.KeyboardButton("Курс валюты")
+        balance_btn = types.KeyboardButton("Остаток на счету")
+        statement_btn = types.KeyboardButton("Выписка")
+        markup.row(rate_btn, balance_btn)
+        markup.row(statement_btn)
 
-    if message.chat.last_name:
-        credentials = f"{message.chat.first_name} {message.chat.last_name}"
+        bot.send_message(
+            chat_id=message.chat.id,
+            text="Главное меню",
+            reply_markup=markup
+        )
     else:
-        credentials = f"{message.chat.first_name} {message.chat.username}"
-    text_message = f"Привет, {credentials}!\nТебя приветствует Finance Bot\n" \
-                   f"{'=' * 25}\n" \
-                   f"На данный момент доступны следующие команды:\n" \
-                   f"/rates - Актуальный курс валют Монобанка\n" \
-                   f"/balance - Текущее состояния счета\n" \
-                   f"/authorization - Запрос авторизации\n" \
-                   f"/statement - Выписка за текущий месяц"
-    bot.send_message(chat_id=message.chat.id, text=text_message)
-    bot.send_message(chat_id=message.chat.id,
-                     text="Доступны следующие опции",
-                     reply_markup=make_keyboard(options_list)
-                     )
+        text_message = "Вы не авторизованы.\nПройдите авторизацию"
+        bot.send_message(chat_id=message.chat.id, reply_markup=make_keyboard_for_unauthorized(), text=text_message)
 
 
-@bot.message_handler(commands=["authorization"])
-def request_for_authorization(message):
-    make_first_user_superuser()
+@bot.message_handler(func=lambda message: message.text == 'Авторизация')
+def authorization_handler(message):
     superuser = User.select().where(User.is_superuser == 1).get(db)
     user = User.select().where(User.chat_id == message.chat.id).get(db)
-
     if user.is_authorized != 1:
         bot.send_message(
             chat_id=superuser.chat_id,
@@ -94,90 +98,72 @@ def request_for_authorization(message):
         )
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def handle_query(call):
-    data = call.data.rsplit(" ")
-    if data[1] != "None":
-        user_identity = int(data[1])
-
-    if call.data.startswith("rates"):
-        text_message = actual_currency_rate()
-        bot.send_message(chat_id=call.message.chat.id, reply_markup=make_keyboard(options_list), text=text_message)
-
-    if call.data.startswith("balance"):
-        user = User.select().where(User.chat_id == call.message.chat.id).get(db)
-        if user.is_authorized == 1:
-            text_message = balance_info()
-            bot.send_message(chat_id=call.message.chat.id, reply_markup=make_keyboard(options_list), text=text_message)
-        else:
-            text_message = "Вы не авторизованы.\nИспользуйте команду /authorization"
-            bot.send_message(chat_id=call.message.chat.id, reply_markup=make_keyboard(options_list), text=text_message)
-
-    if call.data.startswith("accept"):
-        user_to_authorize = User.select().where(User.chat_id == user_identity).get(db)
-        user_to_authorize.is_authorized = 1
-        user_to_authorize.save()
-        bot.send_message(
-            chat_id=call.message.chat.id,
-            text="Пользователь авторизован"
-        )
-        bot.send_message(
-            chat_id=user_identity,
-            text="Поздравляю, Вы авторизованы, все опции доступны."
-        )
-        bot.send_message(
-            chat_id=user_identity,
-            text="Доступны следующие опции",
-            reply_markup=make_keyboard(options_list),
-            parse_mode='HTML'
-        )
-
-    if call.data.startswith("deny"):
-        bot.send_message(chat_id=call.message.chat.id, text="Пользователю отказано в авторизации")
-        bot.send_message(chat_id=user_identity, text="Извините, Вам отказано в авторизации")
-
-    if call.data.startswith("statement"):
-        user = User.select().where(User.chat_id == call.message.chat.id).get(db)
-        if user.is_authorized == 1:
-            bot.send_message(chat_id=call.message.chat.id, text="Готовлю выписку за текущий месяц!")
-            bot.send_message(chat_id=call.message.chat.id, parse_mode="HTML",
-                             text=f'''<pre>{tables.create_table()}</pre>''')
-        else:
-            text_message = "Вы не авторизованы.\nИспользуйте команду /authorization"
-            bot.send_message(chat_id=call.message.chat.id, reply_markup=make_keyboard(options_list), text=text_message)
+@bot.message_handler(func=lambda message: message.text == 'Выписка')
+def statement_handler(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    week_btn = types.KeyboardButton("Выписка за неделю")
+    month_btn = types.KeyboardButton("Выписка за 30 дней")
+    cur_month_btn = types.KeyboardButton("Выписка за текущий месяц")
+    choose_date = types.KeyboardButton("Выписка\nВыбрать день")
+    start_btn = types.KeyboardButton("Меню Старт")
+    markup.row(week_btn, month_btn)
+    markup.row(cur_month_btn, choose_date)
+    markup.row(start_btn)
+    bot.send_message(
+        chat_id=message.chat.id,
+        text="Меню выписки",
+        reply_markup=markup
+    )
 
 
-@bot.message_handler(commands=["rates"])
-def show_currency_rates(message):
-    bot.send_message(chat_id=message.chat.id, text="Я проверю для тебя курсы валют!")
-    text_message = actual_currency_rate()
-    bot.send_message(chat_id=message.chat.id, text=text_message)
-
-
-@bot.message_handler(commands=["balance"])
-def show_currency_rates(message):
-    user = User.select().where(User.chat_id == message.chat.id).get(db)
-    if user.is_authorized == 1:
-        bot.send_message(chat_id=message.chat.id, text="Я проверю для тебя состояние счета!")
-        text_message = balance_info()
-        bot.send_message(chat_id=message.chat.id, text=text_message)
-    else:
-        text_message = "Вы не авторизованы.\nИспользуйте команду /authorization"
-        bot.send_message(chat_id=message.chat.id, reply_markup=make_keyboard(options_list), text=text_message)
-
-
-@bot.message_handler(commands=["statement"])
-def show_statement_for_current_month(message):
+@bot.message_handler(func=lambda message: message.text == 'Выписка за текущий месяц')
+def current_month_statement_handler(message):
     user = User.select().where(User.chat_id == message.chat.id).get(db)
     if user.is_authorized == 1:
         table = tables.create_table()
         bot.send_message(chat_id=message.chat.id, text="Готовлю выписку за текущий месяц!")
         bot.send_message(chat_id=message.chat.id, parse_mode="HTML", text=f'''<pre>{table}</pre>''')
     else:
-        text_message = "Вы не авторизованы.\nИспользуйте команду /authorization"
-        bot.send_message(chat_id=message.chat.id, reply_markup=make_keyboard(options_list), text=text_message)
+        text_message = "Вы не авторизованы.\nПройдите авторизацию"
+        bot.send_message(chat_id=message.chat.id, reply_markup=make_keyboard_for_unauthorized(), text=text_message)
+
+
+@bot.message_handler(func=lambda message: message.text == 'Курс валюты')
+def currency_rates_handler(message):
+    text_message = actual_currency_rate()
+    bot.send_message(chat_id=message.chat.id, text=text_message)
+
+
+@bot.message_handler(func=lambda message: message.text == 'Остаток на счету')
+def actual_balance_handler(message):
+    user = User.select().where(User.chat_id == message.chat.id).get(db)
+    if user.is_authorized == 1:
+        text_message = balance_info()
+        bot.send_message(chat_id=message.chat.id, text=text_message)
+    else:
+        text_message = "Вы не авторизованы.\nПройдите авторизацию"
+        bot.send_message(chat_id=message.chat.id, reply_markup=make_keyboard_for_unauthorized(), text=text_message)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def handle_query(call):
+    data = call.data.rsplit(" ")
+    if data[1] != "None":
+        user_identity = int(data[1])
+
+    if call.data.startswith("accept"):
+        user_to_authorize = User.select().where(User.chat_id == user_identity).get(db)
+        user_to_authorize.is_authorized = 1
+        user_to_authorize.save()
+        bot.send_message(chat_id=call.message.chat.id, text="Пользователь авторизован")
+        bot.send_message(chat_id=user_identity, text="Поздравляю, Вы авторизованы, все опции доступны.")
+
+    if call.data.startswith("deny"):
+        bot.send_message(chat_id=call.message.chat.id, text="Пользователю отказано в авторизации")
+        bot.send_message(chat_id=user_identity, text="Извините, Вам отказано в авторизации")
 
 
 if __name__ == '__main__':
+    bot.remove_webhook()
     bot.infinity_polling()
 
